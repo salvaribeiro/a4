939eafaa841e4a2e79ad4f3db7a979cd36b66585
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2014-04-29 18:52:08 -0300

    Código conforme criado (padrão) pelo IntelliJ.

---
+++ src/br/ufg/inf/fs/android/atividade/Principal.java
@@ -0,0 +1,15 @@
+package br.ufg.inf.fs.android.atividade;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Principal extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
